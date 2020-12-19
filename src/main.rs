9fb9019dd3c2cf613618d6d51c70c969bccc8862
VentGrey <omarpurataf@gmail.com>
2020-12-19 02:17:33 -0600

    Se lindaron los clientes a la función main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+mod clients;
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -17,10 +19,7 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().configure(clients::init_routes)
     })
     .bind("127.0.0.1:8080")?
     .run()
